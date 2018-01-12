93228ffc3ff957e9e4eb45f565f0004c557e625e
pitiboy <peter.horvath@finnoconsult.at>
2018-01-12 09:00:22 +0100

    Adding custom CSS and injecting new elements into DOM by JS for Compact mode

--- deployDev.sh
+++ deployDev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir ~/Library/"Application Support"/Franz/recipes/dev/recipe-mailfejlesztesek
-cp -f ./*.png ./*.svg ./*.json ./*.js ~/Library/"Application Support"/Franz/recipes/dev/recipe-mailfejlesztesek
+cp -f ./*.png ./*.svg ./*.json ./*.js ./*.css ~/Library/"Application Support"/Franz/recipes/dev/recipe-mailfejlesztesek
 
 echo
 echo deployed WebmailFejlesztesek recipe for Franz 'dev'
--- webview.js
+++ webview.js
@@ -25,6 +25,35 @@ module.exports = (Franz) => {
     Franz.setBadge(count);
   };
 
+  function initDom () {
+    // create a new div element
+    var newDiv = document.createElement("span");
+    // and give it some content
+    var newLink = document.createElement("a");
+    newLink.setAttribute('class', 'button-disable');
+    newLink.onclick=function(e) {console.log('clicked', e)};
+    newLink.setAttribute('href', 'javascript:document.body.classList.toggle(\'compact\');');
+
+    var newContent = document.createTextNode("Toggle compact mode!");
+    newLink.appendChild(newContent);
+
+    // add the text node to the newly created div
+    newDiv.appendChild(newLink);
+    newDiv.className='item compact-view';
+
+    // add the newly created element and its content into the DOM
+    document.body.appendChild(newDiv);
+    // NOTE: this should be delayed until page load
+    // document.querySelector(".toolbar").appendChild(newDiv);
+
+    // document.body.classList.add('compact');
+  }
+
+  const path = require('path');
+  Franz.injectCSS(path.join(__dirname, 'style.css'));
+  initDom();
+
+
   // check for new messages every second and update Franz badge
   Franz.loop(getMessages);
 };
